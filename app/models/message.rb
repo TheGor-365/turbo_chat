235648436b5d22c75301a628851ccb34266b2a68
Gor <7495500@gmail.com>
2023-02-03 09:46:26 +0300

    Adding notifications with notice gem

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,4 +2,21 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   after_create_commit { broadcast_append_to room }
+
+  # has_noticed_notifications model_name: 'Notification'
+  # has_many :notifications, through: :user, dependent: :destroy
+  #
+  # after_create_commit :notify_recipient
+  # before_destroy :cleanup_notifications
+  # has_noticed_notifications model_name: 'Notification'
+  #
+  # private
+  #
+  # def notify_recipient
+  #   MessageNotification.with(message: self, message: message).deliver_later(message.user)
+  # end
+  #
+  # def cleanup_notifications
+  #   notifications_as_message.destroy_all
+  # end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   after_create_commit { broadcast_append_to "users" }
 
   has_many :messages
+  # has_many :notifications, as: :recipient, dependent: :destroy
 end
