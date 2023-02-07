f29497ddac55a0594eb00b08ace1abae8bb4bc6f
Gor <7495500@gmail.com>
2023-02-07 21:20:52 +0300

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+gor = User.create(
+  email: 'gor@gor.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 'admin'
+)
+
+eric = User.create(
+  email: 'eric@eric.com',
+  password: 'password',
+  password_confirmation: 'password',
+  role: 'admin'
+)
+
+one = User.create(
+  email: 'one@one.com',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
+gor.joined_rooms << Room.create(name: 'General', is_private: false)
+gor.joined_rooms << Room.create(name: 'Testing', is_private: false)
